1dc38a919c2d2305fa31fba7cdc50c3e370cee72
GoshaDev01 <acaatro21@gmail.com>
2026-05-21 17:32:46 +0300

    Update Theme.js

--- Theme.js
+++ Theme.js
@@ -3,8 +3,6 @@ $(() => {
     const SCHEMA_LIGHT = 'light';
     const SCHEMA_DARK = 'dark';
 
-    const pathCss = '/css';
-
     let currentSchema;
 
     const getSchema = () => localStorage.getItem('schema');
@@ -39,19 +37,14 @@ $(() => {
         const icon = $('.theme-icon');
     
         if (currentSchema === SCHEMA_LIGHT) {
-    
             icon
                 .removeClass('bi-sun-fill')
                 .addClass('bi-moon-stars-fill');
-    
         } else {
-    
             icon
                 .removeClass('bi-moon-stars-fill')
                 .addClass('bi-sun-fill');
-    
         }
-    
     };
 
     const changeTable = (schema) => {
@@ -59,27 +52,18 @@ $(() => {
         const tables = $('.table');
 
         if (tables.length) {
-
             $.each(tables, (index, element) => {
-
                 if (schema === SCHEMA_LIGHT) {
-
                     $(element)
                         .removeClass('table-dark')
                         .addClass('table-light');
-
                 } else {
-
                     $(element)
                         .removeClass('table-light')
                         .addClass('table-dark');
-
                 }
-
             });
-
         }
-
     };
 
     const loadCss = (file, callback) => {
@@ -90,39 +74,26 @@ $(() => {
             .attr({
                 id: "theme-css",
                 rel: "stylesheet",
-                href: `css/${file}?t=${Date.now()}`
+                href: `${file}?t=${Date.now()}`  // <-- изменено: убран путь css/
             });
 
         link.on('load', () => {
-
             if (callback) callback();
-
         });
 
         $('head').append(link);
-
     };
 
     (() => {
-
         currentSchema = getSchema();
-
         if (!currentSchema) {
-
             currentSchema = SCHEMA_LIGHT;
-
             setSchema(currentSchema);
-
         }
-
         loadCss(getFileSchema(), () => {
-
             updateThemeIcon();
-
             changeTable(currentSchema);
-
         });
-
     })();
 
 });
