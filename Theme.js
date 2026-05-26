bf49d058a00caee08eb37576b2a055ee5dd72e00
GoshaDev01 <acaatro21@gmail.com>
2026-05-26 13:36:09 +0300

    Update Theme.js

--- Theme.js
+++ Theme.js
@@ -1,60 +1,70 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     const SCHEMA_LIGHT = 'light';
     const SCHEMA_DARK = 'dark';
 
-    let currentSchema;
+    let currentSchema =
+        localStorage.getItem('schema') || SCHEMA_LIGHT;
 
-    const getSchema = () => localStorage.getItem('schema');
-
-    const setSchema = (schema) =>
+    const setSchema = (schema) => {
         localStorage.setItem('schema', schema);
+    };
 
-    const getFileSchema = () =>
-        `${currentSchema}Theme.css`;
+    const getThemeFile = () => {
+        return `${currentSchema}Theme.css`;
+    };
 
     const updateThemeIcon = () => {
         const icon = $('.theme-icon');
+
         if (currentSchema === SCHEMA_LIGHT) {
-            icon.removeClass('bi-sun-fill').addClass('bi-moon-stars-fill');
+            icon.removeClass('bi-sun-fill')
+                .addClass('bi-moon-stars-fill');
         } else {
-            icon.removeClass('bi-moon-stars-fill').addClass('bi-sun-fill');
+            icon.removeClass('bi-moon-stars-fill')
+                .addClass('bi-sun-fill');
         }
     };
 
     const changeTable = (schema) => {
+
         const tables = $('.table');
-        if (tables.length) {
-            $.each(tables, (index, element) => {
-                if (schema === SCHEMA_LIGHT) {
-                    $(element).removeClass('table-dark').addClass('table-light');
-                } else {
-                    $(element).removeClass('table-light').addClass('table-dark');
-                }
-            });
+
+        if (schema === SCHEMA_LIGHT) {
+
+            tables.removeClass('table-dark')
+                  .addClass('table-light');
+
+        } else {
+
+            tables.removeClass('table-light')
+                  .addClass('table-dark');
         }
     };
 
-    // Обработчик кнопки
-    $('.toggle').off('click').on('click', function() {
-        currentSchema = currentSchema === SCHEMA_LIGHT ? SCHEMA_DARK : SCHEMA_LIGHT;
+    const applyTheme = () => {
+
+        const themeLink = $('#theme-css');
+
+        themeLink.attr('href', getThemeFile());
+
+        updateThemeIcon();
+
+        changeTable(currentSchema);
+    };
+
+    $('.toggle').on('click', function () {
+
+        currentSchema =
+            currentSchema === SCHEMA_LIGHT
+                ? SCHEMA_DARK
+                : SCHEMA_LIGHT;
+
         setSchema(currentSchema);
-        loadCss(getFileSchema(), function() {
-            updateThemeIcon();
-            changeTable(currentSchema);
-        });
+
+        applyTheme();
     });
 
-    // Инициализация
-    currentSchema = getSchema();
-    if (!currentSchema) {
-        currentSchema = SCHEMA_LIGHT;
-        setSchema(currentSchema);
-    }
-    
-    loadCss(getFileSchema(), function() {
-        updateThemeIcon();
-        changeTable(currentSchema);
-    });
+    applyTheme();
 
 });
